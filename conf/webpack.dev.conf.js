8855bcc237a0ef440a086373154270a8acecc450
match14 <match14@yandex.ru>
2024-01-24 15:12:52 +0300

    alkohol

--- conf/webpack.dev.conf.js
+++ conf/webpack.dev.conf.js
@@ -9,7 +9,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: baseWebpackConfig.externals.paths.dist,
-    port: 8081,
+    port: 8084,
     overlay: {
       warnings: true,
       errors: true
